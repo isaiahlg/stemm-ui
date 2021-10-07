028395ccaa20f3c044d9b8a996470bb50a8c65d6
Isaiah LG <ilyonsgalante@powergen-re.com>
2021-10-07 18:04:45 +0300

    Restart app with material ui, create menu bar.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
-import React from "react";
-import { Tabs, Tab, Button, Row, Col, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import {
+  Typography,
+  Toolbar,
+  Box,
+  AppBar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
 import "./App.css";
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
   return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Link href="/home">Home</Nav.Link>
-      <Nav.Link eventKey="/technical">Technical</Nav.Link>
-      <Nav.Link eventKey="/financial">Financial</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav>
+    <Box>
+      <AppBar>
+        <Toolbar>
+          <IconButton onClick={toggleOpen}>
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6">
+            {" STEMM UI"}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={open}>
+        <Toolbar>
+          <IconButton onClick={toggleOpen}>
+            <ChevronLeftIcon />
+          </IconButton>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <DashboardIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <WbSunnyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Solar PV" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <BatteryChargingFullIcon />
+              </ListItemIcon>
+              <ListItemText primary="Batteries" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <LocalAtmIcon />
+              </ListItemIcon>
+              <ListItemText primary="Financials 1" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <LocalAtmIcon />
+              </ListItemIcon>
+              <ListItemText primary="Financials 2" />
+            </ListItem>
+          </List>
+        </Toolbar>
+      </Drawer>
+    </Box>
   );
 }
 
